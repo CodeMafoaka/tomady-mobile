272082e6647a673dd373533c674908fa977f1168
ImRanto <hei.ranto.2@gmail.com>
2026-07-25 16:20:40 +0300

    feat: initialize base application structure with RootLayout and index screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,17 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function HomeScreen() {
+export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-900">
-      <Text className="text-xl font-bold text-white">
-        NativeWind est configuré !
-      </Text>
+    <View style={styles.container}>
+      <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
