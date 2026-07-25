b794946b54bd9ee968bb2a7a51925522c978a563
ImRanto <hei.ranto.2@gmail.com>
2026-07-25 16:25:35 +0300

    feat: initialize project structure with Tailwind CSS integration and root layout setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,16 +2,9 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 bg-white flex flex-col justify-center items-center">
+      <Text className="font-bold text-red-500 text-lg">Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
